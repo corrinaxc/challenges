d1480d0005b8f23a4b7e25b82e4f01fa20d99709
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-09 16:09:41 +0100

    completed savings calculator

--- Javascript/JS-Variables-and-Numbers/track-saving-goals/index.js
+++ Javascript/JS-Variables-and-Numbers/track-saving-goals/index.js
@@ -2,4 +2,5 @@ const startingBalance = 1000;
 let monthlyContribution = 50;
 let months = 24;
 
-
+let totalSavings = startingBalance + (monthlyContribution * months)
+console.log("After " + (months) + " months of saving, you will have $" + (totalSavings) + " in your account")
