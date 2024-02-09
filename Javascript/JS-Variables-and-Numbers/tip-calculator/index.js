72c42e3b040f6b9de9848a2cc98d18c93bd72fe2
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-09 15:55:32 +0100

    completed tip calculator

--- Javascript/JS-Variables-and-Numbers/tip-calculator/index.js
+++ Javascript/JS-Variables-and-Numbers/tip-calculator/index.js
@@ -1,4 +1,8 @@
-let mealCost = 100
-let tipPercentage = 0.1
+let mealCost = 100;
+let tipPercentage = 10;
 
-tipAmount = mealCost *= tipPercentage
+let tipAmount = mealCost * (tipPercentage/100);
+
+let totalCost = mealCost + tipAmount;
+console.log(totalCost);
+console.log("The total cost of the meal is: " + (totalCost) + " euros");
