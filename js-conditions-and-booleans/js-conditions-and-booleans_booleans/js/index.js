75bdeca984a4849836067648f8863d31237d2375
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-12 15:16:51 +0100

    initial answers

--- js-conditions-and-booleans/js-conditions-and-booleans_booleans/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 console.log(Boolean("Try it out"));
+
+// == non-strict and === is strict
+
+// 1. 2 == '2' True - non strict
+// 2. 2 === "2" False - strict
+// 3. 2 === 2 True - strict (same type)
+// 4. "hello world" True - string
+// 5. "" False - empty string
+// 6. true True - true
+// 7. 0 False - zero integer
+// 8. -50 True - non zero integer
+// 9. 50 True - non sero integer
+// 10. 5 > 1 True - logic
+// 11. "1 > 10" True - non  empty string
+// 12. "false" True - non empty string
