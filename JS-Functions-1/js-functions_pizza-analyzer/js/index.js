18afd49db81c63a5873b63fd76ba144fa35eb361
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-14 13:55:56 +0100

    updated functions

--- JS-Functions-1/js-functions_pizza-analyzer/js/index.js
+++ JS-Functions-1/js-functions_pizza-analyzer/js/index.js
@@ -12,60 +12,44 @@ pizzaInput1.addEventListener("input", () => {
    let pizzaSize1 = pizzaInput1.value;
    let pizzaSize2 = pizzaInput2.value;
    calculatePizzaGain(pizzaSize1, pizzaSize2);
-   updatePizzaDisplay(pizzaElement, newSize);
+   updatePizzaDisplay(pizza1, pizzaSize1);
+   updateOutputColor(pizzaSize1, pizzaSize2)
 });
 
 pizzaInput2.addEventListener("input", () => {
    let pizzaSize1 = pizzaInput1.value;
    let pizzaSize2 = pizzaInput2.value;
-   calculatePizzaGain(pizzaSize1, pizzaSize2)
+   calculatePizzaGain(pizzaSize1, pizzaSize2);
+   updatePizzaDisplay(pizza2, pizzaSize2);
+   updateOutputColor(pizzaSize1, pizzaSize2)
 });
 
-// Task 1
-// define the function calculatePizzaGain here
-
-/*### 1 Calculate the Pizza Gain
-
-First, we want to calculate the difference of the two pizza areas for two given pizza diameters. For that, do the following:
-
-1. Write a function `calculatePizzaGain` which expects the two diameters of the two pizzas: `diameter1` and `diameter2`. (Not the radii of the pizzas!)
-2. Inside this function, calculate the areas of both pizzas.
-   > 💡 you can find the formula for that online. The value of pi can be accessed via `Math.PI`.
-3. Calculate the pizza gain in percent relative to the first pizza
-   > 💡 The formula is: `(area2 - area1) / area1 * 100`.
-4. Set the `textContent` of the output element to that value. Use `Math.round(calculatedValue)` to round the pizza gain to the next whole integer value.
-5. Inside both event listeners, save the current values of both pizza inputs in two variables `pizzaSize1` and `pizzaSize2`
-   > 💡 You can use the queried elements and get their value like this: `pizzaInput.value`
-6. Now call the function and pass in the two pizza sizes as the arguments. */
-
 function calculatePizzaGain(diameter1, diameter2) {
-  let area1 = diameter1 * Math.PI;
-  let area2 = diameter2 * Math.PI;
-  let pizzaGain = Math.round((area2 - area1) / area1 * 100);
-  output.value = pizzaGain
+   let area1 = diameter1 * Math.PI;
+   let area2 = diameter2 * Math.PI;
+   let pizzaGain = Math.round((area2 - area1) / area1 * 100);
+   output.value = pizzaGain
+ }
+
+ function updatePizzaDisplay(pizzaElement, newSize) {
+   const newDisplaySize = (newSize / 24) * 100;
+   pizzaElement.style.width = newDisplaySize + "px";
+ }
+ 
+function updateOutputColor(size1, size2) {
+   if (size1 <= size2) {
+      outputSection.style.backgroundColor = "var(--green)"
+   } else {
+      outputSection.style.backgroundColor = "var(--red)"
+   }
 }
 
-// calculatePizzaGain(24, 28)
+ /* 1. Write a function `updateOutputColor` with two parameters: `size1` and `size2`.
+ 2. Set the background color of the `outputSection` to "var(--red)", if we loose pizza. Otherwise, the background color should be set to "var(--green)"
+ 3. Use this function in both event listeners with the correct sizes as arguments.
+ 
+    > 💡 `var(--<variable-name>)` is how you can use predefined variables in CSS. In this case, both color codes are stored in the variables `--red` and `--green` on the `:root` element of the HTML document (have a look into the CSS file!).*/
 
-// Task 2
-/*Great! Now we know how much pizza we gain if we choose the second pizza. But we don't have any visual relations between these two numbers. So lets adapt the pizza displays.
-
-1. Write a function `updatePizzaDisplay` which has two parameters: `pizzaElement` - the queried DOM element of the respective pizza and `newSize` which is the new size of that pizza.
-2. Calculate the new display size of the pizza element
-   > 💡 The formula is: `newSize / 24 * 100`.
-3. Set the width of the `pizzaElement` to this new value.
-   > 💡 You can use `pizzaElement.style.width` for that.
-   > 💡 The width needs a unit, a simple number is not enough. Don't forget to concat "px" to your calculated number.
-4. Call this function in both event listeners and pass in the correct pizza element and the correct pizza size.*/
-
-// define the function updatePizzaDisplay here
-
-// divide the current width by 24 and multiply by the new number?? 
-
-function updatePizzaDisplay(pizzaElement, newSize){
-   let newDisplaySize = 
-   pizzaElement.style.width = (newSize / 24 * 100) + 'px'; 
-}
 
 
 // Task 3
