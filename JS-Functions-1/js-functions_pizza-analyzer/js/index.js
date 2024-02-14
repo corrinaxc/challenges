db695019548bb47c63cf01de2d3e56135e67887f
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-14 17:52:57 +0100

    changed const to let

--- JS-Functions-1/js-functions_pizza-analyzer/js/index.js
+++ JS-Functions-1/js-functions_pizza-analyzer/js/index.js
@@ -9,25 +9,25 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-   let pizzaSize1 = pizzaInput1.value;
-   let pizzaSize2 = pizzaInput2.value;
+   const pizzaSize1 = pizzaInput1.value;
+   const pizzaSize2 = pizzaInput2.value;
    calculatePizzaGain(pizzaSize1, pizzaSize2);
    updatePizzaDisplay(pizza1, pizzaSize1);
    updateOutputColor(pizzaSize1, pizzaSize2)
 });
 
 pizzaInput2.addEventListener("input", () => {
-   let pizzaSize1 = pizzaInput1.value;
-   let pizzaSize2 = pizzaInput2.value;
+   const pizzaSize1 = pizzaInput1.value;
+   const pizzaSize2 = pizzaInput2.value;
    calculatePizzaGain(pizzaSize1, pizzaSize2);
    updatePizzaDisplay(pizza2, pizzaSize2);
    updateOutputColor(pizzaSize1, pizzaSize2)
 });
 
 function calculatePizzaGain(diameter1, diameter2) {
-   let area1 = (diameter1/2) * (diameter1/2) * Math.PI;
-   let area2 = (diameter2/2) * (diameter2/2) * Math.PI;
-   let pizzaGain = Math.round((area2 - area1) / area1 * 100);
+   const area1 = (diameter1/2) * (diameter1/2) * Math.PI;
+   const area2 = (diameter2/2) * (diameter2/2) * Math.PI;
+   const pizzaGain = Math.round((area2 - area1) / area1 * 100);
    output.value = pizzaGain
  }
 
