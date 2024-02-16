f8b82fcdc5bc8411d1f4cca8d34d0687ff158a2f
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-16 14:10:19 +0100

    1. complete

--- JS-Forms/js-forms_checkbox-input/js/index.js
+++ JS-Forms/js-forms_checkbox-input/js/index.js
@@ -14,16 +14,19 @@ function showTosError() {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  if(!data.tos){
+    showTosError();
+    return
+}
   alert("Form submitted");
 });
 
 
+
+// the logic of this is to get the value elements from the form and then work out if the checkbox is ticked - the following
+// statement will be defined through an if statement 
 /* # Checkbox Input
 
 TOS is short for Terms of Service. Here, the `alert()` in the submit event handler should only be called if the `tos` checkbox has been checked.
