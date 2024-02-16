b9a3672e78d146886f98c779ed3659db2604e7ca
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-16 11:14:55 +0100

    added submit functionality

--- JS-Forms/js-forms_handle-form-submit/js/index.js
+++ JS-Forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,22 @@
 console.clear();
+/*# Handle Form Submit
+
+Use JavaScript to react to the form submission.
+
+Log all form data (in object form) into the console in the submit event handler.
+
+Make sure the form is reset and the First Name input is focused after submission.*/
 
 const form = document.querySelector('[data-js="form"]');
+const button = document.querySelector('[data-js="submit-button"]');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const userData = Object.fromEntries(formData);
+    console.log(userData);
+    event.target.elements.firstName.focus();
+    event.target.reset();
+});
+
+
