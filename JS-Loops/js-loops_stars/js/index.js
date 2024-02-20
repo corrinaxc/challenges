b9cecaa7652f08740d41b4cd03e221c2fcfd39ba
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-20 21:59:56 +0100

    added EventListener for clicking on stars

--- JS-Loops/js-loops_stars/js/index.js
+++ JS-Loops/js-loops_stars/js/index.js
@@ -16,17 +16,12 @@ function renderStars(x) {
     else {
       star.src = ("assets/star-empty.svg")
     }
+    star.addEventListener("click", () => {
+      renderStars(i); 
+    })
     starContainer.append(star);
 
   //--^-- your code here --^--
 }}
 
-renderStars(0);
-
-/* Now we want to render a certain number of filled stars, depending on the argument given to the function:
-
-- Add a new parameter to the `renderStars` function, named `filledStars`. Expect its value to be a number.
-- Inside the loop, as you create each star, use the value of `filledStars` to decide if a filled or an empty star should be created.
-  💡 Use the counter variable of the for loop to check which star is currently being created (the first, second, third...)
-- Depending on whether a filled or an empty star should be created, set the `src` of the image element to the filled or empty star image URL.
-- Change the code that calls `renderStars`: pass in a number between 0 and 5 as an argument, and see if it works as expected!*/
+renderStars();
