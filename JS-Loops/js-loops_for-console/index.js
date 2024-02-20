4589fd80985e0cc3119834453c871366c6d9fbd4
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-20 15:19:01 +0100

    logs numbers 0-100

--- JS-Loops/js-loops_for-console/index.js
+++ JS-Loops/js-loops_for-console/index.js
@@ -1,10 +1,5 @@
 console.clear();
-/* Log the numbers from 0 to 100 (including) into the console. Use a for loop.
 
-Make sure your for loop has
-
-- an internal counter variable
-- a criterion to be met by the counter variable
-- a rule how the counter variable is increased
-
-Please switch to the `./index.js` file to start counting! */
+for (let counter = 0; counter <= 100; counter++) {
+    console.log("counter = ", counter);
+}
