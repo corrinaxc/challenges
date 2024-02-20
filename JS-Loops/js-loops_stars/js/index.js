afb27565eb4767abcdc4b08481e794e6dbd55ea4
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-20 17:20:46 +0100

    Task #1 render stars

--- JS-Loops/js-loops_stars/js/index.js
+++ JS-Loops/js-loops_stars/js/index.js
@@ -3,12 +3,18 @@ console.clear();
 const starContainer = document.querySelector('[data-js="star-container"]');
 
 function renderStars() {
+
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
 
+  for (let i = 1; i <= 5; i++) {
+    let star = document.createElement("img");
+    star.src = ("assets/star-empty.svg");
+    starContainer.append(star);
+
   //--^-- your code here --^--
-}
+}}
 
 renderStars();
