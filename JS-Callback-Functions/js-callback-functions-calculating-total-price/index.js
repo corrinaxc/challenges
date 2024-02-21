f80f4f44af2bb3305443b277916ebaa213be94ef
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-21 13:28:34 +0100

    calculate total price

--- JS-Callback-Functions/js-callback-functions-calculating-total-price/index.js
+++ JS-Callback-Functions/js-callback-functions-calculating-total-price/index.js
@@ -1,17 +1,3 @@
-// hi
-
-/* You have an array of products with prices, and you want to calculate the total price of all products using a callback function.
-
-Create a function called calculateTotalPrice that takes two parameters products (an array of products) and extractPrice (a callback function).
-Inside the calculateTotalPrice function, initialize a variable totalPrice to keep track of the total price and set its initial value to 0.
-Use a loop, such as a for...of loop, to iterate through the array of products.
-In each iteration of the loop, call the provided callback function with the current product as an argument to extract its price.
-Add the extracted price to the totalPrice variable in each iteration of the loop.
-After looping through all the products, return the total price as the result of the calculateTotalPrice function.
-
-Create the callback function extractPrice, which should take a product as input and return its price.
-Use the calculateTotalPrice function with the array of products and the callback function to calculate the total price.*/
-
 const products = [
     { name: "Saffron", price: 50 },
     { name: "Cinnamon", price: 6 },
@@ -21,14 +7,19 @@ const products = [
     { name: "Garam Masala", price: 10 },
     { name: "Vanilla Bean", price: 15 },
     { name: "Coriander", price: 6 },
-  ];
+];
+
+function extractPrice(product) {
+    return product.price;
+}
 
 function calculateTotalPrice(products, extractPrice) {
     let totalPrice = 0;
-    for (let key in products) {
-        totalPrice = totalPrice + products[key].price;
+    for (let i = 0; i < products.length; i++) {
+        totalPrice += extractPrice(products[i]);
     }
     console.log("Total Price:", totalPrice);
+    return totalPrice;
 }
 
-calculateTotalPrice(products);
+calculateTotalPrice(products, extractPrice);
