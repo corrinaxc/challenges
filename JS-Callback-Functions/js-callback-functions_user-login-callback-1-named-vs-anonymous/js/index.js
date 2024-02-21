793fcbf5bdc3ac5f15ca4de294a240a8d61db2c9
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-21 10:29:12 +0100

    1. Welcome Message through Higher Function

--- JS-Callback-Functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ JS-Callback-Functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -5,4 +5,18 @@ function handleUserLogin(onSuccess) {
   onSuccess();
 }
 
+function showWelcomeMessage () {
+  console.log("Welcome! You are logged in now.");
+}
+
+handleUserLogin(showWelcomeMessage);
+
 // Call handleUserLogin below!
+
+/*
+You should write a callback function in two different ways.
+In both cases your callback function should log the following text to the console:
+"Welcome! You are logged in now."
+
+1. Write a function with the "function" keyword named "showWelcomeMessage" which logs the desired text. Then
+   call the higher order function "handleUserLogin" and pass your callback function "showWelcomeMessage" as the first argument.*/
