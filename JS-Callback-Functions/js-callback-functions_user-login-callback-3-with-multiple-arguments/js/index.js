b7ca4bf861d68a912e0f29883172bdb224ff1ed8
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-21 11:05:54 +0100

    callback as anonymous function

--- JS-Callback-Functions/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ JS-Callback-Functions/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -12,14 +12,6 @@ function showWelcomeMessage(userName, userRole) {
 
 handleUserLogin(showWelcomeMessage);
 
-/* You should write a callback function in two different ways.
-In both cases your callback function should accept two parameters: "userName" and "userRole".
-The following message should be logged to the console:
-"Welcome ${userName}! You are logged in now as ${userRole}."
-
-1.  Write a function with the "function" keyword named "showWelcomeMessage".
-    Call "handleUserLogin" and pass your callback function as the first argument.
-    (You should see the log from your callback message)
-
-2.  Call "handleUserLogin" again but this time write the callback function as an anonymous function **inside** the function call (i.e. between the round brackets).
-    (You should see the log from your callback message) */
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+})
