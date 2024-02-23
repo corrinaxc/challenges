1a11bc51afafa65c84fae4c62ef66a2536efa28c
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-23 10:57:41 +0100

    export and import of getRandomColo()

--- JS-Structure/js-structure_shapes/index.js
+++ JS-Structure/js-structure_shapes/index.js
@@ -1,3 +1,5 @@
+import getRandomColor from "./utils/randomColor.js";
+
 console.clear();
 
 const root = document.getElementById("root");
@@ -6,18 +8,21 @@ const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
   circle.style.backgroundColor = "#ccc";
+  getRandomColor();
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
   square.style.backgroundColor = "#ccc";
+  getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
   pentagon.style.backgroundColor = "#ccc";
+  getRandomColor();
 });
 
 root.append(circle, square, pentagon);
