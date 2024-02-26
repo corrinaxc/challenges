bbf2d896c21f41392d56c8e491e934724375116a
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-26 16:43:46 +0100

    complete

--- Jan-challenge/index.js
+++ Jan-challenge/index.js
@@ -6,6 +6,10 @@ const person = {
   // [ [ 'name', 'Rick' ], [ 'age', 78 ] ]
   
   function keyValuePairs(obj) {
-    let personArray = []
-    
-  }
+    let personArray = [];
+    for (let key in obj) {
+    personArray.push([key, obj[key]]);}
+    return personArray;
+  };
+
+  console.log(keyValuePairs(person));
