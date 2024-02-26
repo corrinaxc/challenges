cc02e58fa37a6a425f16a9cad8cc6d2934ebe87c
Corrina Cornish <corrina.cornish@gmail.com>
2024-02-26 17:42:46 +0100

    completed challenge

--- Jan-challenge/index.js
+++ Jan-challenge/index.js
@@ -1,15 +1,42 @@
+// 1
+
 const person = {
     name: 'Rick',
     age: 78
   }
   
-  // [ [ 'name', 'Rick' ], [ 'age', 78 ] ]
-  
   function keyValuePairs(obj) {
-    let personArray = [];
+    let personArray = []; //iterate over object - get key and value
     for (let key in obj) {
     personArray.push([key, obj[key]]);}
     return personArray;
   };
 
   console.log(keyValuePairs(person));
+
+
+// 2
+
+const nums = [1, 2, 3, 4]
+
+const beatles = ['john', 'paul', 'george', 'ringo']
+
+function myMap(arr, callback) {
+  const result = [];
+  for (let el of arr) {
+    const changed = callback(el);
+    result.push(changed);
+  }
+  return result;
+}
+
+const doubled = myMap(nums, function(el) {
+  return el * 2
+})
+
+const prefixedBeatles = myMap(beatles, function(el) {
+  return 'Mr ' + el
+ }) 
+
+console.log(doubled);
+console.log(prefixedBeatles);
