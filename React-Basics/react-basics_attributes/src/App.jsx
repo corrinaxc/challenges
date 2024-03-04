fc871d5f1b4305f8244dd30323ed1fc4eb755dc8
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-04 15:51:12 +0100

    removed comments

--- React-Basics/react-basics_attributes/src/App.jsx
+++ React-Basics/react-basics_attributes/src/App.jsx
@@ -12,16 +12,3 @@ function CreateArticle() {
     <a className="article__link" href="https://en.wikipedia.org/wiki/Red_panda">Click here to learn more about the Red Panda!</a>
   </article>
 }
-
-
-// Your component should contain the following parts:
-
-// - an `<article>` as a wrapper HTML element with the class `article`
-// - an `<h2>` with the class `article__title` and a text of your choice
-// - a `<label>` and `<input>` tag connected with `id` and `htmlFor` (!) attributes
-// - an `<a>` tag with the class `article__link` as well as text content and `href` attribute of your choice (What about a Wikipedia article?)
-
-// ## Notes
-
-// - You rendered Article component does not have any styling? Remember that you need to use `className` instead of `class` in JSX.
-// - You only have to touch the [App.jsx](src/App.jsx) file.
