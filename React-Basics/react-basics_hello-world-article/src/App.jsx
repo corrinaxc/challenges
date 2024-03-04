a9e1235eaeeb323d52ca3f25c9bc5a87722b0252
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-04 15:52:11 +0100

    removed comments

--- React-Basics/react-basics_hello-world-article/src/App.jsx
+++ React-Basics/react-basics_hello-world-article/src/App.jsx
@@ -10,15 +10,3 @@ function HelloWorldArticle() {
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, accusamus!</p>
   </article>
 }
-
-// Render an HTML `article` tag containing a heading and a paragraph to the browser. To do so, create a component called `HelloWorldArticle`.
-
-// Use the following hints as guideline:
-
-// - In the [App.jsx](src/App.jsx), create a new function called `HelloWorldArticle()`.
-// - `HelloWorldArticle()` should return an HTML `article` tag as a wrapper which contains;
-//   - an `h1` heading
-//   - at least one `p` tag
-//   - Note: Add whatever tags you like, but the predefined styles are limited ;)
-// - In the return statement of your `App` function, replace the `div` with your `HelloWorldArticle` component.
-// - Once again, look at the browser and be amazed!
