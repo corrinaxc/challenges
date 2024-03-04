634a19f8d2ced26e553b43dc2ec592473f37405d
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-04 15:19:12 +0100

    React Generated Article
    (also changed CSS for h1 to be visible)

--- React-Basics/react-basics_hello-world-article/src/App.jsx
+++ React-Basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,24 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return < HelloWorldArticle/>
 }
+
+function HelloWorldArticle() {
+  return <article>
+    <h1>This is a React generated article!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, accusamus!</p>
+  </article>
+}
+
+// Render an HTML `article` tag containing a heading and a paragraph to the browser. To do so, create a component called `HelloWorldArticle`.
+
+// Use the following hints as guideline:
+
+// - In the [App.jsx](src/App.jsx), create a new function called `HelloWorldArticle()`.
+// - `HelloWorldArticle()` should return an HTML `article` tag as a wrapper which contains;
+//   - an `h1` heading
+//   - at least one `p` tag
+//   - Note: Add whatever tags you like, but the predefined styles are limited ;)
+// - In the return statement of your `App` function, replace the `div` with your `HelloWorldArticle` component.
+// - Once again, look at the browser and be amazed!
