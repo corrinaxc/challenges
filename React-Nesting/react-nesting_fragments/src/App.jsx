3ab0b34da43943bf905adbfc6f8d3c39d4f488ae
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-05 15:42:58 +0100

    rendered fragmented boxes

--- React-Nesting/react-nesting_fragments/src/App.jsx
+++ React-Nesting/react-nesting_fragments/src/App.jsx
@@ -3,10 +3,17 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
-    <main className="flex-container">
+  <main className="flex-container">
+      <Boxes />
+  </main>
+  );
+}
+
+function Boxes() {
+  return (
+  <>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
-  );
+  </>);
 }
