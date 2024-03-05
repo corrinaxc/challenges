a9da8032ee1cfb1366166297efb6fa0fed018535
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-05 11:19:31 +0100

    created button

--- React-Props/react-props_button/src/App.jsx
+++ React-Props/react-props_button/src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <CreateButton color = "pink" disabled={false} text="click meee"/>
+}
+
+function CreateButton({color, disabled, text}) {
+   return (<button style={{height: "100px", backgroundColor: color}} disabled={disabled}>
+   {text}
+  </button>)
 }
