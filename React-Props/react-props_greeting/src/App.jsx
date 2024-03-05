ce9a3ac5449cdbd8e06304c3d5b9e04354ea9b25
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-05 10:36:12 +0100

    updated to work for non coach

--- React-Props/react-props_greeting/src/App.jsx
+++ React-Props/react-props_greeting/src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <Greeting name="Klaus"/>
+  return <Greeting name="Sinta"/>
 }
 
 function Greeting({name}) {
-  return (<h1>{name === ("Jan", "Sven", "Marcell", "Gimena", "Klaus") ? "Hello Coach" : "Hello, {name}"}</h1>);
+  return (<h1>{name === ("Jan", "Sven", "Marcell", "Gimena", "Klaus") ? "Hello Coach" : "Hello, " + name}</h1>);
 }
