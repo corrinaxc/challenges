d2e0aae90f4f0657fa29c0aeedf8902848fd88e2
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-07 15:15:55 +0100

    Entries component

--- React-Project-Setup/Journal-App/src/Components/Entries/Entries.jsx
+++ React-Project-Setup/Journal-App/src/Components/Entries/Entries.jsx
@@ -1,6 +1,11 @@
 export default function Entries() {
     return (
         <div>
+            <nav>
+                <a>All Entries</a>
+                <a>Favourites</a>
+            </nav>
+        
 
         </div>
     )
