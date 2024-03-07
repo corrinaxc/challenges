0f7812697edcfd2599169235c6cf283d845f7b34
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-07 11:30:51 +0100

    rendered fruits and colors using map()

--- React-Arrays/react-with-arrays_fruits/src/App.jsx
+++ React-Arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,64 +1,42 @@
 import Card from "./components/Card";
 import "./App.css";
 
-const fruits = [
-  { id: 1337,
-  name: '🍌 Banana',
-  color: 'yellow',
-  }
-  { id: 1338,
-    name: '🍇 Grapes',
-  color: 'purple',
-  }
-  { id: 1339,
-  name: '🥥 Coconut',
-  color: 'brown',
-  }
-  { id: 1340,
-  name: '🥝 Kiwi',
-  color: 'green',
-  }
-  { id: 1341,
-  name: '🥭 Mango',
-  color: 'yellow',
-  }
-  { id: 1342,
-  name: '🍒 Cherry',
-  color: 'red',
-  }
-  { id: 1343,
-  name: '🍉 Watermelon',
-  color: 'pink',
-  }
-]
-
 export default function App() {
-  const fruits = [];
+ const fruits = [
+    { id: 1337,
+    name: '🍌 Banana',
+    color: 'yellow',
+    },
+    { id: 1338,
+      name: '🍇 Grapes',
+    color: 'purple',
+    },
+    { id: 1339,
+    name: '🥥 Coconut',
+    color: 'brown',
+    },
+    { id: 1340,
+    name: '🥝 Kiwi',
+    color: 'green',
+    },
+    { id: 1341,
+    name: '🥭 Mango',
+    color: 'yellow',
+    },
+    { id: 1342,
+    name: '🍒 Cherry',
+    color: 'red',
+    },
+    { id: 1343,
+    name: '🍉 Watermelon',
+    color: 'pink',
+    }
+  ]
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({id, name, color}) => (<Card key={id} name={name} color={color}/>
+       ))};
     </div>
   );
 }
-
-
-
-
-
-
-// ## Task
-
-// Create an array 'fruits'. It should contain at least 5 objects which all have the properties 'name', 'id', 'color', e.g.:
-
-// ```js
-// {
-//   id: 1337,
-//   name: '🍌 Banana',
-//   color: 'yellow',
-// }
-// ```
-
-// - Hint: the ids need to be unique.
-
-// Use the array method `map` to create a Card component for each fruit in your array. Use the name of each object as the text of the component and the id as the key prop.
