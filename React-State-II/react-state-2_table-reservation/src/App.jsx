429ca119ab45a8911a3b6e7435a44ee383e72cb0
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-11 11:17:02 +0100

    restaurant reservation counter complete

--- React-State-II/react-state-2_table-reservation/src/App.jsx
+++ React-State-II/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,25 @@
 import Counter from "./components/Counter";
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div className="container">
-      <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      const [people, setPeople] = useState(0);
+      function handleIncrement () {
+        setPeople(people + 1)};
+
+      function handleDecrement () {
+        if (people > 0) {
+        setPeople(people-1);
+      }
+      };
+      return (
+        <div className="container">
+          <h1>Place a Table Reservation</h1>
+          <Counter 
+                  people={people}
+                  onIncrement={handleIncrement}
+                  onDecrement={handleDecrement}/>
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
