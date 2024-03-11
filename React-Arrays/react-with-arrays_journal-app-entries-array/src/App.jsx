d354ebd444e109e401575c67cfc6eb6713307661
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-11 10:36:43 +0100

    file upload

--- React-Arrays/react-with-arrays_journal-app-entries-array/src/App.jsx
+++ React-Arrays/react-with-arrays_journal-app-entries-array/src/App.jsx
@@ -1,20 +1,62 @@
-import "./App.css";
-import EntriesSection from "./components/EntriesSection/index";
-import EntryForm from "./components/EntryForm/index";
-import Footer from "./components/Footer/index";
-import Header from "./components/Header/index";
+import "./components/EntriesSection/EntriesSection.css";
 
-function App() {
+import Entry from "./components/Entry/index.jsx"
+import Tabs from "./components/Tabs/index.jsx";
+import Tab from "../Tab";
+import Badge from "../Badge";
+
+export default function EntriesSection() {
+  const entries = [
+    {
+      id: 1000,
+      date: "Feb 5, 2025",
+      motto: "We are in a state of chaos",
+      notes:
+        "Today I learned about React State. It was fun! I can't wait to learn more.",
+    },
+    {
+      id: 999,
+      date: "Feb 4, 2025",
+      motto: "Props, Props, Props",
+      notes:
+        "Today I learned about React Props. Mad props to everyone who understands this!",
+    },
+    {
+      id: 998,
+      date: "Feb 3, 2025",
+      motto: "How to nest components online fast",
+      notes:
+        "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    },
+    {
+      id: 997,
+      date: "Feb 2, 2025",
+      motto: "I'm a React Developer",
+      notes: "My React-ion when I learned about React: 😍",
+    },
+  ];
+
   return (
-    <div className="app">
-      <Header />
-      <main className="app__main">
-        <EntryForm />
-        <EntriesSection />
-      </main>
-      <Footer />
-    </div>
+    <section className="entries-section">
+      <Tabs>
+        <Tab active>
+          All Entries <Badge isActive>3</Badge>
+        </Tab>
+        <Tab>
+          Favorites <Badge>1</Badge>
+        </Tab>
+      </Tabs>
+      <div className="entries-section__entries">
+        {entries.map(({ id, date, motto, notes }, index) => (
+          <Entry
+            key={id}
+            date={date}
+            motto={motto}
+            notes={notes}
+            isLastEntry={index === entries.length - 1}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
-
-export default App;
