538b21844379092534324ffb43ed8dea6d8a2186
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-11 11:36:27 +0100

    colored counter

--- React-State-II/react-state-2_colored-number/src/components/Counter.jsx
+++ React-State-II/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,17 +1,23 @@
 import ColoredNumber from "./ColoredNumber";
 import "./Counter.css";
+import { useState } from "react";
 
 
 export default function Counter() {
+  const [number, setNumber] = useState(0);
+  function handleClick(event) {
+    setNumber(event.target.innerHTML === "+" ? number + 1 : number - 1);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={number} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={handleClick}
         >
           +
         </button>
@@ -19,6 +25,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={handleClick}
         >
           -
         </button>
