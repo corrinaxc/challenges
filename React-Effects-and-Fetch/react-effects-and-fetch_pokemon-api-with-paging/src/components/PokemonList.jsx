30774f0900b21888ac5eb880687527a3ffa6fc48
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-12 11:41:03 +0100

    added paging logic and hid prev on first page

--- React-Effects-and-Fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ React-Effects-and-Fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -4,27 +4,38 @@ import "./PokemonList.css";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState([0])
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
         );
         const data = await response.json();
         setPokemon(data.results);
+        setOffset(offset);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [offset]);
+
+   const handleNext = () => {
+    setOffset((prevState) => prevState + 20)
+   }
+
+   const handlePrev = () => {
+    if (offset >= 20) {
+    setOffset((prevState) => prevState - 20)}
+   }
 
   return (
     <main>
-      <button type="button" className="button">Previous Page</button>
-      <button type="button" className="button">Next Page</button>
+      {offset >= 20 && (<button type="button" className="button" onClick={handlePrev}>Previous Page</button>)}
+      <button type="button" className="button" onClick={handleNext}>Next Page</button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name} className="pokemon">{name}</li>
