cf8608246b6b16fca2c77e658cdc0d380846158c
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-20 12:04:31 +0100

    wip - input test

--- React-Component-Testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ React-Component-Testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,17 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+    render(<Input />);
+    const label = screen.getByRole("label");
+    const input = screen.getByRole("input");
+    expect(label).toBeInTheDocument;
+    expect(input).toBeInTheDocument;
+    expect(label).toHaveAttribute(name, "Name of Game");
+    expect(input).toHaveAttribute(placeholder, "e.g. Dodelido");
+});
 
-test("calls callback on every user input", async () => {});
+test("calls callback on every user input", async () => {
+    const user = userEvent.setup()
+    render(<Input />) 
+});
