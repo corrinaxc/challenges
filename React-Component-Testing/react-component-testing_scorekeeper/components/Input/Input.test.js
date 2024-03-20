6f42c3cf42693fe5f0c8b6dc4eb9312ac7dd75f5
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-20 14:17:37 +0100

    corrections

--- React-Component-Testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ React-Component-Testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -4,15 +4,22 @@ import Input from ".";
 
 test("renders a label and an input with the correct attributes", () => {
     render(<Input />);
-    const label = screen.getByRole("label");
-    const input = screen.getByRole("input");
-    expect(label).toBeInTheDocument;
-    expect(input).toBeInTheDocument;
-    expect(label).toHaveAttribute(name, "Name of Game");
-    expect(input).toHaveAttribute(placeholder, "e.g. Dodelido");
+    const label = screen.getByRole("label", );
+    const input = screen.getByRole("textbox");
+    expect(label).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(label).toHaveAttribute('htmlFor', "Name of Game");
+    expect(input).toHaveAttribute('placeholder', "e.g. Dodelido");
 });
 
 test("calls callback on every user input", async () => {
+    const onChange = jest.fn;
     const user = userEvent.setup()
-    render(<Input />) 
+    render(<Input onChange={onChange}></Input>) 
+
+    const input = screen.getByRole("textbox")
+
+    await user.type(input, "my game")
+
+    expect(onChange).toHaveBeenCalled()
 });
--- React-Component-Testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ React-Component-Testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -8,9 +8,12 @@ jest.mock("next/router", () => ({
 }));
 
 test("renders with two links 'Play' and 'History'", () => {
-  render(<Navigation />);
-  const links = screen.getAllByRole("link");
-  expect(links.length).toBe(2);
-  expect(links[0]).toHaveTextContent("Play");
-  expect(links[1]).toHaveTextContent("History")
+  render(<Navigation></Navigation>);
+  const playLink = screen.getAllByRole("link", {name: "Play"
+});
+  const historyLink = screen.getAllByRole("link", {name: "History"
+});
+
+expect(playLink).toBeInTheDocument();
+expect(historyLink).toBeInTheDocument();
 });
