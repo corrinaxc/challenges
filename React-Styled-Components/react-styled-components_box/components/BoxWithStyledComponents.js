8ac46de04848b118222e424077a5f61a2707853f
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-20 16:04:40 +0100

    boxes with styled and horizontal rendering

---
+++ React-Styled-Components/react-styled-components_box/components/BoxWithStyledComponents.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export default function BoxWithStyledComponents( { $isBlack } ) {
+
+    return (
+        <div>
+        <StyledBox $isBlack={ $isBlack } />
+        </div>
+    )
+}
+
+const StyledBox = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: green;
+    margin: 2rem;
+    background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
+    
+    &:hover {
+        background-color: red;
+    }
+`;
