253ed8875fb05ed4172b0c95f3bdee437e9b8899
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-21 11:07:32 +0100

    light count homepage

--- React-Global-State/react-global-state_lights/pages/_app.js
+++ React-Global-State/react-global-state_lights/pages/_app.js
@@ -1,11 +1,36 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+
+const lightsOn = [
+  {id: 1, name:"Living Room", isOn: false},
+  {id: 2, name:"Kitchen", isOn: false},
+  {id: 3, name:"Bedroom", isOn: false},
+  {id: 4, name:"Bathroom", isOn: false},
+  {id: 5, name:"Garage", isOn: false},
+  {id: 6, name:"Porch", isOn: false},
+  {id: 7, name:"Garden", isOn: false},
+  {id: 8, name:"Office", isOn: false},
+]
 
 export default function App({ Component, pageProps }) {
+  const [isOn, setIsOn] = useState(lightsOn);
+
+  const lightCount = isOn.filter(light => light.isOn).length;
+  // const lightCountSum = lightCount.reduce((a, b) => a + b);
+  console.log(lightCount);
+  // console.log(lightCountSum);
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component 
+      {...pageProps}
+      isOn={isOn}
+      lightCount = {lightCount}
+      // lightCountSum={lightCountSum} 
+      />
     </Layout>
   );
 }
