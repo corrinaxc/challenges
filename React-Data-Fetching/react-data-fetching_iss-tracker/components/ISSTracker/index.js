7a3587f2be93fdf71bee8569bcc862a228687e01
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-23 16:42:31 +0100

    fetch SWR

--- React-Data-Fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ React-Data-Fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,44 +1,55 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
-
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+  const { data, error, isLoading } = useSWR(URL, fetcher)
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  console.log(data);
+  const longitude = data.longitude;
+  const latitude = data.latitude;
   return (
+//   const [coords, setCoords] = useState({
+//     longitude: 0,
+//     latitude: 0,
+//   });
+
+//   async function getISSCoords() {
+//     try {
+//       const response = await fetch(URL);
+//       if (response.ok) {
+//         const data = await response.json();
+//         setCoords({ longitude: data.longitude, latitude: data.latitude });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       getISSCoords();
+//     }, 5000);
+
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={longitude} latitude={latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={longitude}
+        latitude={latitude}
+        // onRefresh={getISSCoords}
       />
     </main>
   );
