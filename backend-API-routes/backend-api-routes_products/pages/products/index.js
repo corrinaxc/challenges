125ae5ecbf45c371dc1a35e9c87fc7858d5aca18
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-02 16:30:25 +0200

    all products listing page

---
+++ backend-API-routes/backend-api-routes_products/pages/products/index.js
@@ -0,0 +1,24 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export default function Products() {
+const { data } = useSWR(`/api/products`, fetcher);
+console.log("data", data);
+if (!data) {
+    return <div>Loading.....</div>
+}
+
+return (
+    <>
+    <ul>
+        {data.map((product) => (
+            <li key={product.data}>
+                <h1>{product.name}</h1>
+                <p>{product.description}</p>
+                <p>price: {product.currency}{product.price}</p>
+                <p>category: {product.category}</p>
+            </li>
+        ))}
+    </ul>
+    </>
+)};
