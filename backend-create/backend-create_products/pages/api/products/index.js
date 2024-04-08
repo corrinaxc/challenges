26957e409c51f844bb6a940c790bcc1a4110c6c2
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-08 15:57:11 +0200

    backend create

---
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -0,0 +1,23 @@
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const products = await Product.find();
+    return response.status(200).json(products);
+  }
+
+  else if (request.method === "POST") {
+    try {
+      const productData = request.body
+      await Product.create (productData);
+
+      response.status(201).json({ status : "Product Created"});
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message})
+    }
+  }
+};
