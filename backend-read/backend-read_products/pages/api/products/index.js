5b9f67861abb427f16691ebb266917fa275eb62a
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-08 11:59:07 +0200

    all products

--- backend-read/backend-read_products/pages/api/products/index.js
+++ backend-read/backend-read_products/pages/api/products/index.js
@@ -1,10 +1,11 @@
-import dbConnect from "@/pages/db/connect";
-import Product from "@/components/Product";
+import dbConnect from "../../db/connect";
+import Product from "../../db/models/product.js";
 
 export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
-    const products = await products.find();
+    const products = await Product.find();
+    console.log(products);
     return response.status(200).json(products);
   } else {
     return response.status(405).json({message: "Method Not Allowed"});
--- backend-read/backend-read_products/pages/db/models/product.js
+++ backend-read/backend-read_products/pages/db/models/product.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 const { Schema } = mongoose; 
 
 const productSchema = new Schema ({
-    product: { type: String, required: true },
+    name: { type: String, required: true}, 
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    currency: {type: String, required: true},
   });
 
-const Product = mongoose.models.Joke || mongoose.model("Joke", jokeSchema);
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
